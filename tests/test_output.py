b07e841b9a13898a8abc9b7cd34c8eaf56d6fa6f
stuffbyyuki <kake.19940627@gmail.com>
2024-03-14 07:51:47 -0600

    join test added and added not-check-row-order

--- tests/test_output.py
+++ tests/test_output.py
@@ -20,10 +20,10 @@ def test_agg(lf):
     )
     query = f'''
         select 
-            sum(total_amount),
-            avg(total_amount),
-            min(total_amount),
-            max(total_amount)
+            sum(total_amount) sum,
+            avg(total_amount) avg,
+            min(total_amount) min,
+            max(total_amount) max
         from lf
         ;
     '''
@@ -32,9 +32,10 @@ def test_agg(lf):
     assert_frame_equal(polars_df, duckdb_df)
 
 def test_groupby_agg(lf):
+    group_cols = ['VendorID', 'payment_type']
     polars_df = (
         lf
-        .group_by('VendorID', 'payment_type')
+        .group_by(group_cols)
         .agg(
             sum=pl.col('total_amount').sum(),
             avg=pl.col('total_amount').mean(),
@@ -43,24 +44,27 @@ def test_groupby_agg(lf):
         )
         .collect()
     )
+    print(polars_df.head())
     query = f'''
         select 
             VendorID,
             payment_type,
-            sum(total_amount),
-            avg(total_amount),
-            min(total_amount),
-            max(total_amount)
+            sum(total_amount) sum,
+            avg(total_amount) avg,
+            min(total_amount) min,
+            max(total_amount) max
         from lf
         group by
+            VendorID,
+            payment_type
+        order by 
             VendorID,
             payment_type
         ;
     '''
     duckdb_df = duckdb.sql(query).pl()
-
-    assert_frame_equal(polars_df, duckdb_df)
-
+    
+    assert_frame_equal(polars_df, duckdb_df, check_row_order=False)
 
 def test_window_func(lf):
     polars_df = (
@@ -73,11 +77,62 @@ def test_window_func(lf):
     )
     query = f'''
         select 
-            avg(fare_amount) over(partition by VendorID),
-            dense_rank() over(partition by payment_type order by total_amount desc) 
+            avg(fare_amount) over(partition by VendorID) avg_fare_per_vendor,
+            dense_rank() over(partition by payment_type order by total_amount desc) ttl_amt_rank_per_pay_type 
         from lf
+        ;
+    '''
+    duckdb_df = duckdb.sql(query).pl().with_columns(pl.col('ttl_amt_rank_per_pay_type').cast(pl.UInt32))
+
+    assert_frame_equal(polars_df, duckdb_df, check_row_order=False)
+
+def test_join(lf):
+    base_lf = (
+        lf
+        .with_columns(
+            pl.col('tpep_pickup_datetime').str.to_datetime('%m/%d/%Y %I:%M:%S %p')
+            .dt.month()
+            .alias('pickup_month')
+        )
+    )
+    join_lf = (
+        base_lf
+        .group_by('VendorID', 'payment_type', 'pickup_month')
+        .agg(
+            sum=pl.col('total_amount').sum()
+        )   
+    )
+    polars_df = (
+        base_lf
+        .join(
+            join_lf,
+            on=['VendorID', 'payment_type', 'pickup_month'], 
+            how='inner'
+        )
+        .collect()
+    )
+
+    query = f'''
+
+        with join_data as (
+            select 
+                VendorID,
+                payment_type,
+                pickup_month,
+                sum(total_amount) sum
+            from base_lf
+            group by
+                VendorID,
+                payment_type,
+                pickup_month
+        )
+            
+        select *
+        from base_lf
+        inner join join_data
+            using (VendorID, payment_type, pickup_month) 
         ;
     '''
     duckdb_df = duckdb.sql(query).pl()
 
-    assert_frame_equal(polars_df, duckdb_df)
+    assert_frame_equal(polars_df, duckdb_df, check_row_order=False)
