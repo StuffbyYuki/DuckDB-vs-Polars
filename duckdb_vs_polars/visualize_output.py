946d547ea118b337fa58748bf82cc7a6b2deaceb
stuffbyyuki <kake.19940627@gmail.com>
2024-03-14 11:00:41 -0600

    organize the func

--- duckdb_vs_polars/visualize_output.py
+++ duckdb_vs_polars/visualize_output.py
@@ -2,16 +2,23 @@ import polars as pl
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-    
 def visualize_output(data):
     
     sns.set_style(style=None)
     df = pl.DataFrame(data, schema=['time in seconds', 'query type', 'library'])
     plt.figure(figsize=(20, 8))
-    ax = sns.barplot(x=list(df['query type']), y=list(df['time in seconds']), hue=list(df['library']), errorbar=None, palette=['#FFF208', '#075AFE'])
+    ax = sns.barplot(
+        df,
+        x='query type', 
+        y='time in seconds', 
+        hue='library', 
+        errorbar=None, 
+        palette=['#FFF208', '#075AFE']
+    )
     
     for container in ax.containers:
         ax.bar_label(container)
+
     ax.set(xlabel='', ylabel='Time in Seconds')
     plt.title('DuckDB vs Polars - Speed Comparison')
     plt.savefig('output.png')
